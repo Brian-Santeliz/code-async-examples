16e55aee06fc528a45707a3e079a9d91cdef451c
Brian <brian@gmail.com>
2020-04-25 19:32:08 -0400

    already to push !

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const { taskOne, taskTwo } = require("./tarea");
 
 async function main() {
-  try{
+  try {
     console.time("midiendo el tiempo de espera");
     const value1 = await taskOne();
     const value2 = await taskTwo();
     console.timeEnd("midiendo el tiempo de espera");
-  
+
     console.log(`the values the task one is ${value1}`);
     console.log(`the values the task two is ${value2}`);
-  }catch(e){
-     throw new Error(e);
+  } catch (e) {
+    throw new Error(e);
   }
 }
 
--- tarea.js
+++ tarea.js
@@ -4,21 +4,21 @@ const newSet = util.promisify(setTimeout);
 
 module.exports = {
   async taskOne() {
-   try{
-    throw new Error("algun problema");
-    await newSet(4000);
-    return "un valor";
-   }catch(e){
-    console.log(e)
-   }
+    try {
+      throw new Error("algun problema");
+      await newSet(4000);
+      return "un valor";
+    } catch (e) {
+      console.log(e);
+    }
   },
 
   async taskTwo() {
-   try{
-    await newSet(2000);
-    return "segundo valor";
-   }catch(e){
-    throw new Error(e);
-   }
+    try {
+      await newSet(2000);
+      return "segundo valor";
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 };
