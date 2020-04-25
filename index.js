6c6bebb855051b27ac8c6149e81ce75c50fd28fa
Brian <brian@gmail.com>
2020-04-25 19:31:05 -0400

    format code !

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { taskOne, taskTwo } = require("./tarea");
 
 async function main() {
-  try{
+  try {
     console.time("midiendo el tiempo de espera");
-   const results =  await Promise.all([ taskOne(), taskTwo()])
+    const results = await Promise.all([taskOne(), taskTwo()]);
     console.timeEnd("midiendo el tiempo de espera");
-  
+
     console.log(`the values the task one is  ${[results[0]]}`);
     console.log(`the val ues the task two is ${[results[1]]}`);
-  }catch(e){
-     throw new Error(e);
+  } catch (e) {
+    throw new Error(e);
   }
 }
 
--- tarea.js
+++ tarea.js
@@ -4,22 +4,22 @@ const newSet = util.promisify(setTimeout);
 
 module.exports = {
   async taskOne() {
-   try{
-    throw new Error("algun problema");
-    await newSet(700);
-    return "un valor";
-   }catch(e){
-    console.log(e)
-   }
+    try {
+      throw new Error("algun problema");
+      await newSet(700);
+      return "un valor";
+    } catch (e) {
+      console.log(e);
+    }
   },
 
   async taskTwo() {
-   try{
-    // throw new Error("algun problema");
-    await newSet(100);
-    return "segundo valor";
-   }catch(e){
-    throw new Error(e);
-   }
+    try {
+      // throw new Error("algun problema");
+      await newSet(100);
+      return "segundo valor";
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 };
